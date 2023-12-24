e6ce3ea03c830feca3f69c1ec4fe7c3d8f87df84
Phil Eng <phileng@google.com>
2023-12-24 14:37:32 -0500

    testing vscode git commit and push to gitlab

---
+++ Work/bouncy.py
@@ -0,0 +1,19 @@
+####
+#### bouncy.py
+#### 
+
+print ("hello ")
+
+height = 100
+row = 0
+
+print ("row \t height")
+
+while (row < 10):
+    print (row , height)
+    row = row + 1
+    height = height * .75
+    height = round(height,3)
+
+print (" done ! ")
+    
