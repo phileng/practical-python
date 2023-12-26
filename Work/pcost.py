d9eee3741d467f827bea027d8faa7b6abea3b06e
Phil Eng <phileng@google.com>
2023-12-26 00:48:36 -0500

    using argv

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,20 +1,39 @@
 # pcost.py
 #
 # Exercise 1.27
-# read portfolio.csv, calc cost to buy all shares
-totalcost = 0.0
+import sys
+import os
 
-f = open('Data/portfolio.csv', 'rt')
-headers = next(f).split(',')
+def portfolio_cost(filename):
+    print("entering def")
+# read portfolio.csv, calc cost to buy all shares
+    totalcost = 0.0
+    print(filename)
+#    f = open('Data/portfolio.csv', 'rt')
+    f = open(filename, 'rt')
+    headers = next(f).split(',')
 #print (headers)
 
-for line in f:
-    row = line.split(',')
+    for line in f:
+        row = line.split(',')
     # extract each share's quantity * price and sum it up    
     # print (row[0], row[1], row[2])
-    totalcost = totalcost + (int(row[1]) * float(row[2]))
+        try:
+            totalcost = totalcost + (int(row[1]) * float(row[2]))
+        except ValueError:
+            print("couldnt parse", line)
 
-print ("cost to buy:", totalcost)
+    print ("inside def Total cost:", totalcost)
+    f.close()
 
+print(sys.argv)
+print(os.getcwd())
 
-f.close()
+if len(sys.argv) == 2:
+    filename = sys.argv[1]
+else:
+    filename = '/Users/phileng/src/1)python/practical-python/Work/Data/portfolio.csv'
+print(filename)
+
+portfolio_cost(filename)
+#print('outside def Total cost:', cost)
